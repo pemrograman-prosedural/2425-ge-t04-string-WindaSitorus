efe74bc2b9b849145e8f4f8319282c319c5b8639
WindaSitorus <windasitorus0197@gmail.com>
2025-02-27 14:42:44 +0700

    pair 26

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,25 @@
-// NIM - Name
-// NIM - Name
+// 12S24003 - Michael Pratama Nasution
+// 12S24019 - Winda N.V. Sitorus
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+    char string_input[40];
+    char string_pesan[14];
+    int indeks;
 
-  return 0;
+    scanf("%39s", string_input);
+
+    for (indeks = 0; indeks < strlen(string_input); indeks += 3) {
+        int nilai_ascii = atoi(strncpy((char[4]) {0}, string_input + indeks, 3));
+        string_pesan[indeks / 3] = (char) nilai_ascii;
+    }
+
+    string_pesan[indeks / 3] = '\0';
+
+    printf("%s\n", string_pesan);
+
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,22 @@
-// NIM - Name
-// NIM - Name
+// 12S24003 - Michael Pratama Nasution
+// 12S24019 - Winda N.V. Sitorus
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+    char string_input[50];
+    int indeks;
+
+    fgets(string_input, sizeof(string_input), stdin);
+
+    string_input[strcspn(string_input, "\n")] = 0;
+
+    for (indeks = 0; indeks < strlen(string_input); indeks++) {
+        printf("%03d", string_input[indeks]);
+    }
+
+    printf("013\n");
 
   return 0;
 }
